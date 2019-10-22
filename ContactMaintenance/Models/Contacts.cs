92f2769f9da2b87c5b4f5b52ba8d130102860024
parthapratimsarma <56798249+parthapratimsarma@users.noreply.github.com>
2019-10-22 08:31:10 +0530

    Added regular expression validation for Email

--- ContactMaintenance/Models/Contacts.cs
+++ ContactMaintenance/Models/Contacts.cs
@@ -14,7 +14,8 @@ namespace ContactMaintenance.Models
         [Required]
         public string FirstName { get; set; }
         public string LastName { get; set; }
-
+        
+        [RegularExpression(@"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
         [Required]
         public string Email { get; set; }
 
